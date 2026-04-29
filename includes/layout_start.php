497e803126c27b3b29023f19896389b0c0e2e7d6
shailesh <shaileshbagade@gmail.com>
2026-04-29 12:17:24 +0530

    small logo

--- includes/layout_start.php
+++ includes/layout_start.php
@@ -71,7 +71,7 @@ $isFranchiseOfficerRole = is_franchise_officer_role($user);
             <button class="menu-toggle" type="button" id="menuToggle" aria-expanded="true" aria-controls="appSidebar" aria-label="Toggle menu" title="Toggle menu">
                 <span class="menu-toggle__icon" aria-hidden="true"></span>
             </button>
-            <img src="assets/images/allure-logo.png" alt="<?= e($appName) ?>" class="main__header-logo" loading="eager" decoding="async">
+            <img src="assets/images/allure-logo-small.png" alt="<?= e($appName) ?>" class="main__header-logo" loading="eager" decoding="async">
         </header>
         <div class="main__body">
         <?php
